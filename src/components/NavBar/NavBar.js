f470ff84771b5d11f7d30653ad3bb9bd8db0deb6
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-10 18:07:47 -0500

    init

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NavBar.scss";
+function NavBar() {
+  return (
+    <section className="NavBar">
+      <nav>Some Nav Bar</nav>
+    </section>
+  );
+}
+
+export default NavBar;
