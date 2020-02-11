c2540cafd77c89c85fc675ca63eddf6ee24f2762
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-11 18:46:34 -0500

    building more template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
