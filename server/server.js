d817b5aa68e8c0df505dc08be06182965164d5b4
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-12 19:42:20 -0500

    server oauth

--- server/server.js
+++ server/server.js
@@ -5,16 +5,22 @@ var express = require("express");
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth").OAuthStrategy;
 var cors = require("cors");
-
-// Create a new Express application.
 var app = express();
 
-app.use(cors());
+// Create a new Express application.
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a token, tokenSecret, and Google profile), and
-//   invoke a callback with a user object.
+app.use(cors());
+app.use(require("morgan")("combined"));
+app.use(require("cookie-parser")());
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
 passport.use(
   new GoogleStrategy(
     {
@@ -40,31 +46,9 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 // middlewares which i don't fully understand lol but they are good to have
-app.use(require("morgan")("combined"));
-app.use(require("cookie-parser")());
-app.use(require("body-parser").urlencoded({ extended: true }));
-app.use(
-  require("express-session")({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true
-  })
-);
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.get("/login/facebook", passport.authenticate("facebook"));
-
-app.get(
-  "/return",
-  passport.authenticate("facebook", { failureRedirect: "/login" }),
-  function(req, res) {
-    res.redirect("/");
-  }
-);
 
 app.listen(process.env.PORT || port, () => {
   console.log("Server listening on port", port);
